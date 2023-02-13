b1682b503e2956010de286082ea9f04e23b5168b
Sven Berling <ich@sven-berling.de>
2023-02-13 21:54:14 +0100

    Added Events Listener

--- StarsPlugin/StarsPlugin.js
+++ StarsPlugin/StarsPlugin.js
@@ -5,11 +5,7 @@ import { html,LitElement,css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/l
 export class StarsPlugin extends LitElement {
   
   static properties = {
-    apiKey: {type: String}, 
-    FormattedAdress: {type: String},
-    maptype: {type: String},
-    long: {type: String}
-    
+    outcome: {type: Number}    
   };
 
   static styles = css`
@@ -69,24 +65,29 @@ export class StarsPlugin extends LitElement {
       version: '1.2',
       properties: {
         
-        maptype: {
-          title: 'Map Type',
-          type: 'string',
-        	enum: ['roadmap', 'satellite', 'terrain','hybrid'],
-          showAsRadio: false,
-          verticalLayout: true,
-          defaultValue: 'roadmap',
-          description: 'Choose your Map Type: roadmap,satellite,terrain or hybrid'
-        }         
-        
-      }
+        outcome: {
+          title: 'Rating',
+          type: 'integer',
+        	description: 'Insert a Variable, to save the Rating',
+          isValueField: true
+        },        
+      },
+      events: ["ntx-value-change"],
     };
   }
 
 
-  checkAdress() {
-  }     
-  
+  onChange(e) {
+    const args = {
+        bubbles: true,
+        cancelable: false,
+        composed: true,
+        // value coming from input change event. 
+        detail: e.target.value,
+    };
+    const event = new CustomEvent('ntx-value-change', args);
+    this.dispatchEvent(event);
+}
 
   
   constructor() {
@@ -96,17 +97,25 @@ export class StarsPlugin extends LitElement {
   }
 
 
+  onClick(e) {
+
+      console.log("TEST" +  this.prop);
+
+  }
+
+
   headerTemplate() {
+    
     return html`  <div class="rate">
-    <input type="radio" id="star5" name="rate" value="5" />
+    <input type="radio" id="star5" name="rate" value="5" onclick="${this.onClick()}" />
     <label for="star5" title="text">5 stars</label>
-    <input type="radio" id="star4" name="rate" value="4" />
+    <input type="radio" id="star4" name="rate" value="4" onclick="${this.onClick()}"/>
     <label for="star4" title="text">4 stars</label>
-    <input type="radio" id="star3" name="rate" value="3" />
+    <input type="radio" id="star3" name="rate" value="3" onclick="${this.onClick()}"/>
     <label for="star3" title="text">3 stars</label>
-    <input type="radio" id="star2" name="rate" value="2" />
+    <input type="radio" id="star2" name="rate" value="2" onclick="${this.onClick()}"/>
     <label for="star2" title="text">2 stars</label>
-    <input type="radio" id="star1" name="rate" value="1" />
+    <input type="radio" id="star1" name="rate" value="1" onclick="${this.onClick()}"/>
     <label for="star1" title="text">1 star</label>
   </div>
                 
