b42bf883313ed78db7d2caa3d3a2def42961d751
Sven Berling <ich@sven-berling.de>
2023-02-14 09:02:45 +0100

    Simple Value to function

--- StarsPlugin/StarsPlugin.js
+++ StarsPlugin/StarsPlugin.js
@@ -99,7 +99,7 @@ export class StarsPlugin extends LitElement {
 
   onClick(e) {
 
-      console.log("TEST" +  this.prop);
+      console.log("TEST" +  e);
 
   }
 
@@ -107,15 +107,15 @@ export class StarsPlugin extends LitElement {
   headerTemplate() {
     
     return html`  <div class="rate">
-    <input type="radio" id="star5" name="rate" value="5" onclick="${this.onClick()}" />
+    <input type="radio" id="star5" name="rate" value="5" onclick="${this.onClick(5)}" />
     <label for="star5" title="text">5 stars</label>
-    <input type="radio" id="star4" name="rate" value="4" onclick="${this.onClick()}"/>
+    <input type="radio" id="star4" name="rate" value="4" onclick="${this.onClick(4)}"/>
     <label for="star4" title="text">4 stars</label>
-    <input type="radio" id="star3" name="rate" value="3" onclick="${this.onClick()}"/>
+    <input type="radio" id="star3" name="rate" value="3" onclick="${this.onClick(3)}"/>
     <label for="star3" title="text">3 stars</label>
-    <input type="radio" id="star2" name="rate" value="2" onclick="${this.onClick()}"/>
+    <input type="radio" id="star2" name="rate" value="2" onclick="${this.onClick(2)}"/>
     <label for="star2" title="text">2 stars</label>
-    <input type="radio" id="star1" name="rate" value="1" onclick="${this.onClick()}"/>
+    <input type="radio" id="star1" name="rate" value="1" onclick="${this.onClick(1)}"/>
     <label for="star1" title="text">1 star</label>
   </div>
                 
