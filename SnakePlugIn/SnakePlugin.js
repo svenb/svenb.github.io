eb4a751c542ac017394ebe9aab2a330cea806d8b
Sven Berling <ich@sven-berling.de>
2023-02-20 08:57:48 +0100

    First Upload

---
+++ SnakePlugIn/SnakePlugin.js
@@ -0,0 +1,122 @@
+import { html,LitElement, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+
+
+// define the component
+export class SnakePlugIn extends LitElement {
+  
+  static properties = {
+    Score: {type: String}    
+  };
+  
+  static styles = css`
+
+  body {
+    background: rgb(212, 211, 211);
+    }      
+    .grid {
+        width: 200px;
+        height: 200px;
+        border: 1px solid red;
+        margin: 0 auto;
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .grid div {
+        width: 20px;
+        height: 20px;
+        /*border:1px black solid;
+    box-sizing:border-box*/
+    }
+    .snake {
+        background: blue;
+    }
+    .apple {
+        background: yellow;
+        border-radius: 20px;
+    }
+    .popup {
+        background: rgb(32, 31, 31);
+        width: 100px;
+        height: 100px;
+        position: fixed;
+        top: 100px;
+        left: 100px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+  `;
+
+
+  // return a promise for contract changes.
+  static getMetaConfig() {
+    return {
+      controlName: 'Snake Game',
+      fallbackDisableSubmit: false,
+      groupName: 'Fun',
+      version: '1.2',
+      properties: {
+        Score: {
+          type: 'string',
+          title: 'Score',
+          description: 'Save your Snake Score to your Workflow'
+        }
+      }
+    };
+  }
+
+
+  checkAdress() {
+     
+      
+      return html`
+                
+           
+
+      `;
+    
+    
+  }     
+  
+
+  
+  constructor() {
+    super();
+    this.checkAdress(); 
+    
+  }
+
+
+  headerTemplate() {
+    return html` 
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>       
+      `;
+  }
+
+  footerTemplate() {
+    return html`  `;
+  }
+  
+
+  
+
+  render() {        
+
+    
+    return html`       
+        
+           ${this.headerTemplate()}
+           ${this.checkAdress()} 
+           ${this.footerTemplate()}
+          
+      
+    `;     
+
+  }
+  
+}
+
+// registering the web component
+const elementName = 'snake-plugin';
+customElements.define(elementName, SnakePlugIn);
